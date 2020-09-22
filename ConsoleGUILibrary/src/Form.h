5bd0bc1996d62cff994d5a92503c1b36b0c5d778
Sean Kuehl <teddscottfelt@gmail.com>
2020-09-22 18:34:24 -0400

    initial commit

---
+++ ConsoleGUILibrary/src/Form.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <vector>
+
+
+class Form
+{
+	//the whole thing is based around a large vector of chars
+private:
+
+
+public:
+
+	Form() {
+
+	}
+
+	void AddButton() {
+
+	}
+
+	void AddText() {
+
+	}
+
+	void AddList() {
+		//stretches across cells vertically with text only
+	}
+
+};
+
---
+++ ConsoleGUILibrary/src/Source.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+	//max width is 120 '-'
+	//max hieght before needing scrolling is 28 (including top and bottom things)
+	//every letter is a symbol in length
+	
+	//divide each 'screen' into slots that can be used by single elements
+	//or larger elements can be strung across multiple, a borderline idea
+	//a 'button' is surrounded like a box, a text box doesn't have that etc
+	
+
+	return 0;
+}
